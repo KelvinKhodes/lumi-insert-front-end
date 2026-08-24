dc410aed139f5600239ae58bb4fe768c14f5bf3b
KelvinKhodes <kelvinkho050@gmail.com>
2026-08-24 16:28:05 +0700

    feat: add memo pop up

---
+++ src/lib/components/MemoViewModal.svelte
@@ -0,0 +1,24 @@
+<script> 
+  import Modal from './Modal.svelte'; 
+
+  /**
+   * @typedef {Object} Props
+   * @property {boolean} [open]
+   * @property {any} [memo]
+   * @property {any} [onClose]
+   */
+
+  /** @type {Props} */
+  let { open = false, memo = null, onClose = () => {} } = $props();
+  
+ 
+</script>
+
+<Modal {open} {onClose} title="{memo?.title}" maxWidthClass="max-w-[440px]">
+  <p class="text-ink-secondary whitespace-pre-line">{memo?.body}</p>
+  {#if memo?.images !== null}
+    {#each memo?.images as image}
+      <img src={image} class="mt-2 max-h-[300px] w-full object-contain" />
+    {/each}
+  {/if}
+</Modal>
